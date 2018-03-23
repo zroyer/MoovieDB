80f27a054cf8058acd5642483e0ffe01c0ecab3e
Zach Royer <zro@crowdnoise.com>
2018-03-23 10:39:40 -0500

    adds schema to server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const Movie = require('./model/movies');
 const app = express();
 const router = express.Router();
 const port = process.env.API_PORT || 1738;
