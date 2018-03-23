64a6e4500e1f8158e27a8b2f756705fd8e57de2e
Zach Royer <zro@crowdnoise.com>
2018-03-23 11:14:30 -0500

    adds react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import './index.css'
 import Header from './components/Header'
-import MovieContainer from './containers/MovieContainer'
+import MovieSearch from './containers/MovieSearch'
+import MovieForm from './components/MovieForm'
+import MovieList from './components/MovieList'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <div>
-    <Header />
-    <MovieContainer />
-  </div>,
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/search/" component={MovieSearch} />
+      </Switch>
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
  );
 registerServiceWorker();
