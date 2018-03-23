059059d780d2b9d21b04684ebd750d26b486a235
Zach Royer <zro@crowdnoise.com>
2018-03-23 10:28:39 -0500

    adds headers to server

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const port = process.env.API_PORT || 1738;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,PUT,GET,HEAD,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 router.get('/', function(req, res) {
   res.json({ message: 'bueno 👌'});
 });
